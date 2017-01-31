109285080766c1f3fcba09a17ba1f9f2ab37c275
bengi <bengitiger@gmail.com>
2017-02-01 08:24:45 +0900

    'db-dump'

---
+++ doc/dbdump/db.sql
@@ -0,0 +1,64 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.6.31 : Database - laravel_application
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `laravel_application`;
+
+/*Table structure for table `migrations` */
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (3,'2014_10_12_000000_create_users_table',1),(4,'2014_10_12_100000_create_password_resets_table',1);
+
+/*Table structure for table `password_resets` */
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`),
+  KEY `password_resets_token_index` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `password_resets` */
+
+/*Table structure for table `users` */
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`email`,`avatar`,`slug`,`gender`,`password`,`remember_token`,`created_at`,`updated_at`) values (1,'bengi','bengitiger@gmail.com','public/defaults/avatar/male.jpg','bengi',1,'$2y$10$tktrBPc0g4fo5GRxEgu8YuVRp.Fr0QBfn7WsCJlr5A5R0QWesPOFG','WGVYZcxhdKmurEbXQVWhaOa7wjzNV1QAx0RavqqFmxzE92nhYKKw4fnt53hL','2017-01-30 23:36:26','2017-01-30 23:36:26'),(2,'Lee Bengi Tiger','bengitiger119@gmail.com','public/defaults/avatar/male.jpg','lee-bengi-tiger',1,'$2y$10$Ba0yW6xb2k3/Ac9JsrnLEuaig4dLMR6pe5UXOj3E//xf9h.PR8krG',NULL,'2017-01-30 23:41:04','2017-01-30 23:41:04');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
